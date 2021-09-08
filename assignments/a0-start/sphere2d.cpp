52f6cb207bc3f6dd5080f4fbb4d631fc2336e8f2
foqiashahid112 <foqiashahid112@gmail.com>
2021-09-08 10:19:00 -0400

    Assignment 0 files added

--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -9,13 +9,18 @@ class Sphere2D : public atkui::Framework {
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
-    setColor(vec3(0,1,0));
-
-    // draw a sphere at center of screen
+    setColor(vec3(1,0,0));
     double x = width() * 0.5;
     double y = height() * 0.5;
-    double radius = 100; 
-    drawSphere(vec3(x,y,0), radius);
+
+    //draw a sphere behind center sphereRadius
+    double radius_2 = 200;
+    drawSphere(vec3(x,y,-x), radius_2);
+    
+    //draw a green sphere at center of screen
+    setColor(vec3(0,1,0));
+    double radius = 100;
+    drawSphere(vec3(x,y,0),radius);
   }
 };
 
--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,10 +1,17 @@
 #include "atkui/framework.h"
 
 using glm::vec3;
+using namespace std;
 
 class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
+  }
+
+  virtual void setup(){
+    currentPos = vec3(0,0,0);
+    vel = vec3(0,0,0); //100.0f * agl::randomUnitVector();
+    spacePressed = false;
   }
 
   virtual void scene() {
@@ -13,8 +20,28 @@ class Sphere3D : public atkui::Framework {
 
     // draw a sphere at center of the world
     float radius = 50.0;
-    drawSphere(vec3(0), radius);
+    drawSphere(currentPos, radius);
+    if (spacePressed == true){
+      currentPos = currentPos + vel * dt();
+    }
   }
+
+  //@Override keyUp in Sphere3D
+  void keyUp(int key,int mods){
+    if (key == GLFW_KEY_SPACE){
+      //Green sphere moves in random direction
+      vel = 100.0f * agl::randomUnitVector();
+      spacePressed = true;
+    }
+    if(key == GLFW_KEY_R){
+      //reset position of the sphere to (0,0,0)
+      currentPos = vec3(0,0,0);
+      spacePressed = false;
+    }
+  }
+  bool spacePressed;
+  vec3 currentPos;
+  vec3 vel;
 };
 
 int main(int argc, char** argv)
