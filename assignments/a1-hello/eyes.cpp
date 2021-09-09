aafbd500233c318c1ba35b490356ea5cba0b3b67
Foqia Shahid <foqiashahid112@gmail.com>
2021-09-09 15:35:06 -0400

    question 1

--- assignments/a1-hello/eyes.cpp
+++ assignments/a1-hello/eyes.cpp
@@ -6,9 +6,30 @@ class Eyes : public atkui::Framework
   public:
     Eyes() : atkui::Framework(atkui::Orthographic) {
     }
+    
+    virtual void setup(){
+      theta = 0.0;
+      theta_Rate = 1;
+    }
 
     virtual void scene() {
+      setColor(vec3(1,1,1));
+      drawSphere(vec3(0.3 * width(), 0.5 * height(), 0), 90);
+      drawSphere(vec3(0.7 * width(), 0.5 * height(), 0), 90);
+
+      theta += theta_Rate * dt();
+      float px = 30 * cos(theta);
+      float py = 0.5 * height();
+      setColor(vec3(0,0,0));
+      drawSphere(vec3(0.3 * width() + px, py, 100), 30);
+      drawSphere(vec3(0.7 * width() + px, py, 100), 30);
     }
+    vec3 left;
+    vec3 right;
+    float r;
+    float theta;
+    float theta_Rate;
+    float eyeRadius;
 };
 
 int main(int argc, char** argv) {
