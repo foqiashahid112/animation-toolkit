6d50fdb21b98d6194dcaf709849d8fff6bc1b9b8
foqiashahid112 <foqiashahid112@gmail.com>
2021-09-25 12:06:41 -0400

    updated and added comments for particlecubic.cpp

--- assignments/a2-interpolation/particlecubic.cpp
+++ assignments/a2-interpolation/particlecubic.cpp
@@ -1,57 +1,68 @@
-/* Question 2
-* Implement an application that draws a cubic curve and its control points
-* using both Bernstein polynomials and De Casteljau’s algorithm.
+/* Question 3
+* Animate the position of a particle based on a cubic polynomial.
 */
 
 #include "atkui/framework.h"
 #include <math.h>  /* fmod */
 using namespace glm;
+using namespace std;
 
 class ParticleCubic : public atkui::Framework {
  public:
   ParticleCubic() : atkui::Framework(atkui::Orthographic,  400, 400) {
   }
-  
-  void scene() {
+
+  virtual void setup(){
+    controlPoints.push_back(vec3(100,  50, 0));
+    controlPoints.push_back(vec3(150, 200, 0));
+    controlPoints.push_back(vec3(250, 100, 0));
+    controlPoints.push_back(vec3(300, 300, 0));
+  }
+
+  virtual void scene() {
     drawBernstein();
     drawBall();
   }
-    void drawBernstein(){
-    float t = 0.01;
+
+  /* 
+  * @brief : Function draws Bernstein using helper function Bezier
+  */
+  void drawBernstein(){
+    setColor(vec3(1,1,0));
+    float t = 0.00;
     while( t < 1.00){
-      vec3 p1 = Bern(t);
+      vec3 p1 = Bezier(controlPoints, t);
       t+= 0.01;
-      vec3 p2 = Bern(t);
-      setColor(vec3(1,1,0));
+      vec3 p2 = Bezier(controlPoints, t);
+      if(t > 1.00){ p2 = p1; }
       drawLine(p1,p2);
       }
   }
-
-  vec3 Bern(double t){
-      float bern_1 = 1;
-      float bern_2 = 0;
-      float bern_3 = 0;
-      float bern_4 = 0;
-      bern_1 = pow((1-t),3.0);
-      bern_2 = 3*t*pow((1-t), 2.0);
-      bern_3 = 3 * pow(t,2.0) * (1-t);
-      bern_4 = pow(t, 3.0);
-      return bern_1* B0 + bern_2 * B1 + bern_3 * B2 + bern_4* B3;
+  /* 
+  * @brief : Helper Function calculates Bezier Basis
+  * for cubic curve at time t for given control points
+  */
+  vec3 Bezier(vector<vec3> ctrlpts, float t){
+    float t2 = t * t;
+    float t3 = t2 * t;
+    float mt = 1 - t;
+    float mt2 = mt * mt;
+    float mt3 = mt2 * mt;
+    return ctrlpts[0] * mt3 + 3.0f * ctrlpts[1]*mt2*t + 3.0f *ctrlpts[2] * mt*t2 + ctrlpts[3]*t3;
   }
 
+  /* 
+  * @brief : Function draws animated ball on cubic curve
+  */
   void drawBall(){
     setColor(vec3(1,0,0));
-
     double t = fmod(elapsedTime(), 5.0);
     double T = t / 5.0;
-    vec3 currentPos = Bern(T);
+    vec3 currentPos = Bezier(controlPoints,T);
     drawSphere(currentPos, 10); 
   }
   private:
-  vec3 B0 = vec3(100,  50, 0);
-  vec3 B1 = vec3(150, 200, 0);
-  vec3 B2 = vec3(250, 100, 0);
-  vec3 B3 = vec3(300, 300, 0);
+  vector<vec3> controlPoints;
 };
 
 int main(int argc, char** argv) {
