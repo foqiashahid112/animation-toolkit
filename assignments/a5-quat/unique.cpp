e45a570ef05986213ead31dd07b6fa2133dc3515
foqiashahid112 <foqiashahid112@gmail.com>
2021-10-22 12:27:15 -0400

    incomplete unique

--- assignments/a5-quat/unique.cpp
+++ assignments/a5-quat/unique.cpp
@@ -1,4 +1,6 @@
 #include "atkui/framework.h"
+#include "atkmath/quaternion.h"
+
 using namespace glm;
 
 class Circle{
@@ -18,12 +20,13 @@ public:
    }
 
    virtual void setup(){
-      v = Vector3(0,0,1);
-      angle = 0;
+      v = atkmath::Vector3(0,0,1);
+      //v = vec3(0,0,1);
+      angle = M_PI / 4;
       q.fromAxisAngle(v, angle);
       q_inv = q.inverse();
 
-      Color Palette for circles:
+      //Color Palette for circles:
       pallet = {
          vec3(0,165,227)/255.0f,
          vec3(141,215,191)/255.0f,
@@ -39,35 +42,43 @@ public:
 
    virtual void scene(){
       for(Circle& circle : myCircles){
+         X = 0.5 * width();
+         Y = 0.5 * height();
+         vec3 p_prev = vec3(X,Y,0);
+         vec3 p_new = q * p_prev * q_inv;
          push();
-         drawCircle(circle.col, circle.r); 
+         translate(p_new);
+         drawCircle(vec3(0,0,0), circle.col, circle.r);
          pop();
       }
    }
-   void drawCircle(vec3 color, float radius){
+   void drawCircle(vec3 location, vec3 color, float radius){
       setColor(color);
       float thetaRate = (2.0  * M_PI) / 10.0 ;
       float theta = 0;
       vec3 p1;
       vec3 p2;
       for( int i = 0; i < 20; i++) {
-         float px = radius * cos(theta) + 0.5 * width();
-         float py = radius * sin(theta) + 0.5 * height();
+         float px = radius * cos(theta) + location[0];
+         float py = radius * sin(theta) + location[1];
          float pz = 0;
          p1 = vec3(px, py,pz);
          theta = theta + thetaRate;
-         float px_ = radius * cos(theta) + 0.5 * width();
-         float py_ = radius * sin(theta) + 0.5 * height();
+         float px_ = radius * cos(theta) + location[0];
+         float py_ = radius * sin(theta) + location[1];
          p2 = vec3(px_, py_, pz);
          drawLine(p1, p2);
       }
    }
    private:
+   float X, Y;
    std::vector<vec3> pallet;
    std::vector<Circle> myCircles;
-   Quaternion q;
-   Quaternion q_inv;
-   Vector3 v; double angle;
+   atkmath::Quaternion q;
+   atkmath::Quaternion q_inv;
+   atkmath::Vector3 v;
+   // vec3 v;
+   double angle;
 };
 
 int main(int argc, char** argv) {
