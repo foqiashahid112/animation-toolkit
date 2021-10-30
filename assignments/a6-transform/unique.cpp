d6925a8c0684382ef9ed362e8181a654dded671d
foqiashahid112 <foqiashahid112@gmail.com>
2021-10-29 21:12:24 -0400

    incomplete unique

--- assignments/a6-transform/unique.cpp
+++ assignments/a6-transform/unique.cpp
@@ -38,31 +38,47 @@ public:
         _tentacle.addJoint(joint4, joint3);
         _tentacle.fk(); // compute local2global transforms
 
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 5; i++){
             tentacles.push_back(_tentacle);
+        }
+        // Skeleton ten2;
+        // Joint* nroot  = new Joint("nroot");
+        // nroot->setLocalTranslation(vec3(50,50, 50));
+        // ten2.addJoint(nroot);
+
+        // Joint* njoint1  = new Joint("njoint1");
+        // njoint1->setLocalTranslation(vec3(50,50,50));
+        // ten2.addJoint(njoint1, nroot);
+
+        // tentacles.push_back(ten2);
+
+        for (std::vector<Skeleton>::iterator it = tentacles.begin() ; it != tentacles.end(); ++it){
+            int joints = it->getNumJoints();
+            std::cout << "num of joints " << joints << std::endl;
+        }
    }
      
 
    virtual void scene(){
-    for(Skeleton _tentacle: tentacles){
-        float a = 3;
+       float a = 3;
         float b = 0.2;
         float displacement = 0.2;
-        for(int i = 0; i < _tentacle.getNumJoints(); i++){
-         glm::quat rotation = glm::angleAxis(a * sin(b* elapsedTime()+ i + displacement), glm::vec3(0.0, 0.0f, 1.0f));
-         Joint *j = _tentacle.getByID(i);
-         j->setLocalRotation(rotation);
+    for(int j = 0; j < tentacles.size(); j++){
+        for(int i = 0; i < tentacles.at(j).getNumJoints(); i++){
+            glm::quat rotation = glm::angleAxis(sin(elapsedTime()+ i), glm::vec3(0.0, 0.0f, 1.0f));
+            Joint *j = tentacles.at(j).getByID(i);
+            j->setLocalRotation(rotation);
         }
         displacement += 0.2;
-      _tentacle.fk(); // computes local2global transforms
+      tentacles.at(j).fk(); // computes local2global transforms
     }
       setColor(vec3(0,1,0));
 
       //Draw
-      for(Skeleton _tentacle: tentacles){
-        for(int i = 0; i < _tentacle.getNumJoints() - 1 ; i++){
-            Joint* parent = _tentacle.getByID(i);
-            Joint* child = _tentacle.getByID(i+1);
+      for(int i = 0; i < tentacles.size(); i++){
+        for(int j = 0; j < tentacles.at(i).getNumJoints() - 1 ; j++){
+            Joint* parent = tentacles.at(i).getByID(j);
+            Joint* child = tentacles.at(i).getByID(j+1);
             vec3 globalParentPos = parent->getGlobalTranslation();
             vec3 globalPos = child->getGlobalTranslation(); 
             drawEllipsoid(globalParentPos, globalPos, 10); 
